dd2b733446742ba739e3e54fd3a8f668007c19b7
Tyler Carver <tbcarver@yahoo.com>
2024-05-14 12:35:23 -0600

    recom-1488 Web app was upgraded with scount extended which changed the signature of the scout:import to not use model=

--- src/Console/ElasticMigrateCommand.php
+++ src/Console/ElasticMigrateCommand.php
@@ -274,7 +274,7 @@ class ElasticMigrateCommand extends Command
 
         $this->call(
             'scout:import',
-            ['model' => get_class($sourceModel)]
+            [get_class($sourceModel)]
         );
     }
 
